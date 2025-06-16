49452bfe10bebeac2bddc2ebc8d79f66417b9cf5
siddhant-2006 <siddhantsingh2006sh@gmail.com>
2025-06-16 20:36:21 +0530

    Add files via upload
    git add .
    git commit -m "Add basic password generator script"
    git push origin main

---
+++ passwordgen.py
@@ -0,0 +1,14 @@
+import random
+chars = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+length=int(input("enter length: "))
+password=""
+
+
+
+for a in range(length):
+    password+=random.choice(chars)
+    print("generated password:", password)
+    
+    
+    
+    
